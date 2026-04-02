abd467283bf3ef89ea3ccbd273100d6959647282
Letcia Hartstein Ivnaov <119649477+LeticiaHartstein@users.noreply.github.com>
2026-04-02 00:45:23 -0300

    Add files via upload

---
+++ CalculoDeMedia.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+int main ()
+{
+	//declarando as váriaveis
+	int Nota1  = 0, Nota2 = 0, Nota3 = 0, media = 0;
+	std::string nome = "";
+
+	//inserindo as notas
+	std::cout << "insira a primeira nota: \n";
+	std::cin >> Nota1;
+
+	std::cout << "insira a segunda nota \n";
+	std::cin >> Nota2;
+
+	std::cout << "insira a terceira nota: \n";
+	std::cin >> Nota3;
+
+	//inserindo o nome
+	std::cin.ignore(); //limpando o buffer para não interferir no que for inserido na string
+	std::cout << "insira o nome do aluno: \n";
+	std::getline(std::cin, nome); //usando o getline, para que possa ser armazenado o nome, mesmo com espaço
+
+	media = (Nota1 + Nota2+ Nota3)/3; // Cálculo da média
+
+	if (media < 7 )
+	{
+		std::cout << "o aluno: " << nome << " esta de recuperacao" ;		
+	}
+	else 
+	{
+		std::cout << "o aluno: " << nome << " esta aprovado ";
+	}
+	return 0;
+}
